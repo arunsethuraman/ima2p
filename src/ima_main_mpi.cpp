40bd932ec0e2d12785f7c05b0ffb9743be363fa5
Arun Sethuraman <tuf29140@login3.nfs>
2015-07-24 16:41:00 -0400

    Fixed a small bug that created files with .0 extension. Now L mode can proceed without having to rename these files.

--- src/ima_main_mpi.cpp
+++ src/ima_main_mpi.cpp
@@ -1692,7 +1692,7 @@ void start (int argc, char *argv[], int currentid)
   //  strcpy (genealogyinfosavefilename, outfilename);
   //  strcat (genealogyinfosavefilename, ".ti");
   // strcat (genealogyinfosavefilename, currentid);
-     sprintf(genealogyinfosavefilename, "%s.ti.%d", outfilename, currentid);
+     sprintf(genealogyinfosavefilename, "%s.ti", outfilename);
 	}
   }
 ///AS:TODO cdurationmode has to be broadcast
@@ -1701,7 +1701,7 @@ void start (int argc, char *argv[], int currentid)
   //  strcpy (oldoutfilename, outfilename);
   //  strcat (oldoutfilename, ".old");
   //  strcat (oldoutfilename, currentid);
-      sprintf(oldoutfilename, "%s.old.%d", outfilename, currentid);
+      sprintf(oldoutfilename, "%s.old", outfilename, currentid);
   }
   for (i = 0; i < MAXLOCI; i++)
     hilocuslike[i] = -1e20;
